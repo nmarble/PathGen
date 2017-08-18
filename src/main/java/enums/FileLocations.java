2d38e41cbf24203b19de74b82b256caf15e933b9
nick <nmarble@uccs.edu>
2017-08-17 22:57:21 -0600

    Added elf, halfelf, halforc name gen

--- src/main/java/enums/FileLocations.java
+++ src/main/java/enums/FileLocations.java
@@ -5,7 +5,12 @@ package main.java.enums;
  */
 public enum FileLocations {
     HUMAN_FIRST_NAME("namefiles/Human_First_Names.csv"),
-    HUMAN_LAST_NAME("namefiles/Human_Last_Names.csv");
+    HUMAN_LAST_NAME("namefiles/Human_Last_Names.csv"),
+    ORC_NAMES("namefiles/Orc_Names.csv"),
+    ELF_PREFIX("namefiles/Elf_Prefix.csv"),
+    ELF_SUFFIX("namefiles/Elf_Suffix.csv"),
+    ELF_FAMILY_PREFIX("namefiles/Elf_Family_Prefix.csv"),
+    ELF_FAMILY_SUFFIX("namefiles/Elf_Family_Suffix.csv");
 
     private String name;
     private final String BASELOCATION = "/main/resources/";
--- src/main/java/utils/NameUtils.java
+++ src/main/java/utils/NameUtils.java
@@ -6,12 +6,12 @@ import main.java.enums.RaceType;
 
 import java.util.Collection;
 import java.util.Random;
-
+import static main.java.enums.FileLocations.*;
 /**
  * Created by Nick on 8/17/2017.
  */
 public class NameUtils {
-    Random random = new Random();
+    private static Random random = new Random();
 
     public static String getRandomName(RaceType classType)
     {
@@ -19,6 +19,12 @@ public class NameUtils {
         {
             case HUMAN:
                 return getRandomHumanName();
+            case HALF_ORC:
+                return getRandomOrcName();
+            case ELF:
+                return getRandomElfName();
+            case HALF_ELF:
+                return getRandomHalfElf();
             default:
                 return "RANDOM";
         }
@@ -26,10 +32,77 @@ public class NameUtils {
 
     private static String getRandomHumanName()
     {
-        Collection<String> firstNames = CSVReader.getCSVByLine(FileLocations.HUMAN_FIRST_NAME.getName());
-        String firstName = firstNames.stream().skip((int) (firstNames.size() * Math.random())).findFirst().get();
-        Collection<String> lastNames = CSVReader.getCSVByLine(FileLocations.HUMAN_LAST_NAME.getName());
-        String lastName = lastNames.stream().skip((int) (lastNames.size() * Math.random())).findFirst().get();
-        return firstName + " " + lastName;
+        return getRandom(HUMAN_FIRST_NAME.getName()) + " " + getRandom(HUMAN_LAST_NAME.getName());
+    }
+
+    private static String getRandomOrcName()
+    {
+        switch(random.nextInt(3))
+        {
+            case 0:
+                return getRandom(ORC_NAMES.getName());
+            case 1:
+                return getRandom(HUMAN_FIRST_NAME.getName()) + " " + getRandom(ORC_NAMES.getName());
+            case 2:
+                return getRandom(ORC_NAMES.getName()) + " " + getRandom(HUMAN_LAST_NAME.getName());
+            default:
+                return getRandom(ORC_NAMES.getName());
+        }
+    }
+    private static String getRandomElfName()
+    {
+        return getRandomElfFirst() + " " + getRandomElfLast();
+    }
+
+    private static String getRandom(String file)
+    {
+        Collection<String> list = CSVReader.getCSVByLine(file);
+        String string = list.stream().skip((int) (list.size() * Math.random())).findFirst().get();
+        return string;
+    }
+    private static String getRandomElfFirst()
+    {
+        switch(random.nextInt(3))
+        {
+            case 0:
+                return getRandom(ELF_PREFIX.getName()) + getRandom(ELF_SUFFIX.getName());
+            case 1:
+                return getRandom(ELF_PREFIX.getName()) + getRandom(ELF_SUFFIX.getName()) + getRandom(ELF_SUFFIX.getName());
+            case 2:
+                return getRandom(ELF_SUFFIX.getName()) + "'" + getRandom(ELF_PREFIX.getName()) + getRandom(ELF_SUFFIX.getName()) + getRandom(ELF_SUFFIX.getName());
+            default:
+                return  "";
+        }
+    }
+    private static String getRandomElfLast()
+    {
+        switch(random.nextInt(3))
+        {
+            case 0:
+                return getRandom(ELF_FAMILY_PREFIX.getName()) + getRandom(ELF_FAMILY_SUFFIX.getName());
+            case 1:
+                return getRandom(ELF_FAMILY_PREFIX.getName()) + getRandom(ELF_FAMILY_SUFFIX.getName()) + getRandom(ELF_FAMILY_SUFFIX.getName());
+            case 2:
+                return getRandom(ELF_FAMILY_PREFIX.getName()) + getRandom(ELF_FAMILY_SUFFIX.getName()) + "'" + getRandom(ELF_SUFFIX.getName());
+            default:
+                return "";
+        }
+    }
+
+    private static String getRandomHalfElf()
+    {
+        switch(random.nextInt(4))
+        {
+            case 0:
+                return getRandomElfName();
+            case 1:
+                return getRandom(HUMAN_FIRST_NAME.getName()) + " " + getRandomElfLast();
+            case 2:
+                return getRandomElfFirst() + " " + getRandom(HUMAN_LAST_NAME.getName());
+            case 3:
+                return getRandomHumanName();
+            default:
+                return "";
+        }
     }
 }
